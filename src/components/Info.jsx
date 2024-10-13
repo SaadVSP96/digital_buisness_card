ec3f841efc89d2706150a085a696d10ce7e8e295
SaadVSP96 <saadmasood192@gmail.com>
2024-10-13 15:15:09 +0500

    4. Project finetuning complete

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Info() {
     return (
         <div className="Info--container">
-            <img src="../../images/DSC_5814.JPG" alt="" />
+            <img src="/DSC_5814.JPG" alt="" />
             <h3 className="Info--name">Saad Bin Masood</h3>
             <h4 className="Info--role">Full Stack Developer</h4>
             <a className="Info--mail-btn" href="mailto:saadmasood192@gmail.com">
